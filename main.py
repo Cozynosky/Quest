01081dea6f978c580579a05d143485812a6fd297
Mateusz Kozlowski <ct14kozlowskim@gmail.com>
2021-11-03 22:32:23 +0100

    dodano szablon bazowy base.html oraz obsluge zakladki menu

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "kawiarnia_ksiegarnia_quest"
 
 
+# obsluga strony glownej
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
 
 
 if __name__ == "__main__":
