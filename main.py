b3bd2416baffea9edef0e1ebf984bb00b6c8271f
Mateusz Kozlowski <ct14kozlowskim@gmail.com>
2021-11-04 11:20:41 +0100

    poprawiono szablony stron oraz dodano kilka nowych oraz zakladki

--- main.py
+++ main.py
@@ -11,9 +11,46 @@ def home():
     return render_template("index.html")
 
 
+# obsluga zakladki menu
 @app.route("/menu")
 def menu():
     return render_template("menu.html")
+
+
+# obsluga zakladki kontakt
+@app.route("/kontakt")
+def contact():
+    return render_template("contact.html")
+
+
+# obsluga zakladki o nas
+@app.route("/o_nas")
+def about():
+    return render_template("about.html")
+
+
+# obsluga zakladki koszyk
+@app.route("/koszyk")
+def cart():
+    return render_template("cart.html")
+
+
+# obsluga zakladki ksiegarnia
+@app.route("/ksiegarnia")
+def shop():
+    return render_template("shop.html")
+
+
+# obsluga zakladki konto
+@app.route("/konto")
+def account():
+    return render_template("account.html")
+
+
+# obsluga zakladki wydarzenia
+@app.route("/wydarzenia")
+def events():
+    return render_template("events.html")
 
 
 if __name__ == "__main__":
